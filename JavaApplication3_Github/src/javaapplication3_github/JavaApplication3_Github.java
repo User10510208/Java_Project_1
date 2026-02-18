8e18e97ec6e789e2e9f24bb9bb9ff124ab5793f2
User <User@DESKTOP-I76G7OA>
2026-02-18 11:47:39 +0200

    Added variables, printing (with maths)

---
+++ JavaApplication3_Github/src/javaapplication3_github/JavaApplication3_Github.java
@@ -0,0 +1,21 @@
+package javaapplication3_github;
+
+public class JavaApplication3_Github {
+
+    public static void main(String[] args) {
+        // Variables
+        String stringTest = "Good";
+        String nameTest = "my Friend";
+        
+        int x = 6;
+        int y = 7;
+        
+        // Printing
+        System.out.println("Hello " + nameTest + ".");
+        System.out.print(stringTest);
+        System.out.println("bye!");
+        
+        System.out.println(x+y);
+    }
+    
+}
